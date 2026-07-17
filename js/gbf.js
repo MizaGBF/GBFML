5fe92eb4308f3e024283caf2b3ac523b4b5c2798
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2026-07-17 13:39:46 +0200

    Updated GBFAL css for asset-text and skycompass

--- js/gbf.js
+++ js/gbf.js
@@ -23,6 +23,9 @@ class GBF
 	static c_endpoint = (
 		"https://prd-game-a-granbluefantasy.akamaized.net/"
 	);
+	static c_skycompass_endpoint = (
+		"https://media.skycompass.io/"
+	);
 	static c_eternals = Object.freeze([
 		"3040030000", "3040031000", "3040032000", "3040033000", "3040034000", "3040035000", "3040036000", "3040037000", "3040038000", "3040039000"
 	]);
@@ -54,6 +57,12 @@ class GBF
 	endpoint()
 	{
 		return GBF.c_endpoint;
+	}
+	
+	// getter: return the Sky Compass endpoint
+	skycompass_endpoint()
+	{
+		return GBF.c_skycompass_endpoint;
 	}
 	
 	// getter: return the list of eternal IDs
